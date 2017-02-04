c8db44ba100cadaa40fb0b66956a9b2928b4fc39
Joel Sherrill <joel@rtems.org>
2017-02-04 16:58:06 -0600

    Update coverity scripts to run against master
    Add scripts to test newlib and rtems-tools as well as capture
    the dummy Coverity model files. The website complains if you
    have not filled in all the holes. Providing dummy values gives
    us "completely configured" projects.

---
+++ coverity/rtems-coverity-model.c
@@ -0,0 +1,5 @@
+//
+// RTEMS currently does not have anything to model for Coverity.
+// Having a file makes them happy we have a fully configured project. :)
+//
+ 
---
+++ coverity/rtems-newlib-model.c
@@ -0,0 +1,5 @@
+//
+// RTEMS-newlib currently does not have anything to model for Coverity.
+// Having a file makes them happy we have a fully configured project. :)
+//
+ 
